25f16ed915c09b50523b1005f8596b171ea0830c
GigGun <biggiggun@mail.ru>
2019-04-22 02:05:25 +0500

    Corrects html

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def main(request):
     return render(request, 'mainapp/index.html')
 
@@ -12,3 +13,4 @@ def products(request):
 
 def contact(request):
     return render(request, 'mainapp/contact.html')
+
