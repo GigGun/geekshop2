84c0f056a892273a64e460dd565f73d3a15189af
GigGun <klyushinekb@gmail.com>
2019-05-01 15:43:31 +0500

    add name menu positions

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-context = {'title': 'Вагончик'}
+context = {'title': 'Вагончик',
+           'main': 'Главная',
+           'products': 'Каталог',
+           'contact': 'Контакты', }
 
 
 def main(request):
@@ -10,8 +13,8 @@ def main(request):
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context)
