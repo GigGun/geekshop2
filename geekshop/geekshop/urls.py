7bc4a04340e71c3dc9c05d8cecbf6e519f35b071
GigGun <klyushinekb@gmail.com>
2019-05-02 17:34:36 +0500

    add DEBUG and MEDIA_ROOT

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
