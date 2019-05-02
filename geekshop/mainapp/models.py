df4d2cea787f3a77dec6d54b3f30244a1d20ef60
GigGun <klyushinekb@gmail.com>
2019-05-02 13:00:10 +0500

    add class Meta

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 # Create your models here.
 class ProductCategory(models.Model):
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
     name = models.CharField(verbose_name='имя', max_length=64, unique=True)
     description = models.TextField(verbose_name='описание', blank=True)
 
