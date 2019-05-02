d40bf33547d520cd5903443081cbb049ff3ad528
GigGun <klyushinekb@gmail.com>
2019-05-02 15:38:41 +0500

    add media

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import mainapp.views as mainapp
-
+from django.conf import settings
 
 urlpatterns = [
     path('products/', mainapp.products, name='products'),
@@ -24,3 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.main, name='main'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
