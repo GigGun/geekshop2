21f1e004613aa2324dd2778cc0c0cbd0a03d8880
GigGun <klyushinekb@gmail.com>
2019-05-10 15:55:06 +0500

    add edit and register

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -5,7 +5,8 @@ import authapp.views as authapp
 app_name = 'authapp'
 
 urlpatterns = [
-    # path('register', authapp.register, name='register'),
+    path('register', authapp.register, name='register'),
     path('login/', authapp.login, name='login'),
     path('logout/', authapp.logout, name='logout'),
+    path('edit', authapp.edit, name='edit'),
 ]
