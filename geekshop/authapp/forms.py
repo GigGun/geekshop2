ae5c4447689581d37a53955ad6b6da48a71e7013
GigGun <klyushinekb@gmail.com>
2019-05-10 17:46:50 +0500

    fix "name 'forms' is not defined" error

--- geekshop/authapp/forms.py
+++ geekshop/authapp/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from .models import ShopUser
@@ -40,7 +41,7 @@ class ShopUserEditForm(UserChangeForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_namem, field in self.field.items():
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
             if field_name == 'password':
