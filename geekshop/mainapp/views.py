6ad3bb74d097e53b91c6fde9e2d51d4037d597b5
GigGun <klyushinekb@gmail.com>
2019-05-15 20:19:04 +0500

    added new functionality in products()

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import ProductCategory, Product
+
 
 context = {'title': 'Вагончик',
            'links_menu': {'main': 'Главная', 'products': 'Каталог', 'contact': 'Контакты'},
@@ -15,6 +17,30 @@ def main(request):
 
 def products(request, pk=None):
     print(pk)
+
+    title = 'продукты'
+    links_menu = ProductCategory.objects.all()
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+
+        context = {'title': title,
+                   'links_menu': links_menu,
+                   'category': category,
+                   'products': products, }
+        return render(request, 'mainapp/products.html', context)
+
+    same_products = Product.objects.all()[3:5]
+
+    context = {'title': title,
+               'links_menu': links_menu,
+               'same_products': same_products}
+
     return render(request, 'mainapp/products.html', context)
 
 
