bcdc864e5be1feab387b80d3fdf7b71ad7595bf1
GigGun <klyushinekb@gmail.com>
2019-05-26 14:36:08 +0500

    delete sme print`s

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -16,13 +16,11 @@ def main(request):
 
 
 def products(request, pk=None):
-    print(f'pk={pk}')
 
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
 
     if pk is not None:
-        print(f'pk={pk} True')
         if pk == 0:
             products_obj = Product.objects.all().order_by('price')
             category = {'name': 'все'}
@@ -30,8 +28,6 @@ def products(request, pk=None):
         else:
             category = get_object_or_404(ProductCategory, pk=pk)
             products_obj = Product.objects.filter(category__pk=pk).order_by('price')
-            print(f'{products_obj}')
-            print(f'{category}')
 
         context = {'title': title,
                    'links_menu': links_menu,
@@ -40,7 +36,6 @@ def products(request, pk=None):
         return render(request, 'mainapp/product_list.html', context)
 
     same_products = Product.objects.all()[1:3]
-    print(f'same_products={same_products}')
 
     context = {'title': title,
                'links_menu': links_menu,
