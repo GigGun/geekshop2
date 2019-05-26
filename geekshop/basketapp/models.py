952be6d5ab608ec3178ae47a1b0672b73720945c
GigGun <klyushinekb@gmail.com>
2019-05-26 17:58:39 +0500

    add basketapp

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from mainapp.models import Product
+from authapp.models import ShopUser
 
-# Create your models here.
+
+class BasketSlot(models.Model):
+    class Meta:
+        verbose_name = 'корзина'
+        verbose_name_plural = 'корзины'
+
+    user = models.ForeignKey(ShopUser, on_delete=models.CASCADE, related_name='basket')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+    created = models.DateTimeField(verbose_name='время', auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.product.name
--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from .models import BasketSlot
+from mainapp.models import Product
 
-# Create your views here.
+
+def add(request, pk=None):
+    product = get_object_or_404(Product, pk=pk)
+
+    basket_slot = BasketSlot(product=product, user=request.user)
+    basket_slot.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def delete(request, pk=None):
+    context = {}
+    return render(request, 'basketapp/basket.html', context)
