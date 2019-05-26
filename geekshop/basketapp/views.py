0dc1c885e8e86c8dac7b863d6e7886ec742c4f2e
GigGun <klyushinekb@gmail.com>
2019-05-26 18:20:59 +0500

    add quantity products

--- geekshop/basketapp/views.py
+++ geekshop/basketapp/views.py
@@ -6,12 +6,27 @@ from mainapp.models import Product
 def add(request, pk=None):
     product = get_object_or_404(Product, pk=pk)
 
-    basket_slot = BasketSlot(product=product, user=request.user)
-    basket_slot.save()
+    basket_slot_old = BasketSlot.objects.filter(product=product).first()
+
+    if basket_slot_old:
+        basket_slot_old.quantity += 1
+        basket_slot_old.save()
+    else:
+        basket_slot = BasketSlot(product=product, user=request.user)
+        basket_slot.save()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 def delete(request, pk=None):
-    context = {}
-    return render(request, 'basketapp/basket.html', context)
+    product = get_object_or_404(Product, pk=pk)
+    basket_slot = BasketSlot.objects.filter(product=product).first()
+
+    if basket_slot:
+        if basket_slot.quantity > 1:
+            basket_slot.quantity -= 1
+            basket_slot.save()
+        else:
+            basket_slot.delete()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from .models import ProductCategory, Product
-
+from basketapp.models import BasketSlot
 
 context = {'title': 'Вагончик',
            'links_menu': {'main': 'Главная', 'products': 'Каталог', 'contact': 'Контакты'},
@@ -16,6 +16,12 @@ def main(request):
 
 
 def products(request, pk=None):
+
+    basket = []
+    if request.user.is_authenticated():
+        basket = BasketSlot.objects.filter(user=request.user)
+
+    total_quatity = sum(list(map(lambda basket_slot: basket_slot.quantity, basket())))
 
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
@@ -32,7 +38,8 @@ def products(request, pk=None):
         context = {'title': title,
                    'links_menu': links_menu,
                    'category': category,
-                   'products': products_obj, }
+                   'products': products_obj,
+                   'basket_quantity': total_quatity, }
         return render(request, 'mainapp/product_list.html', context)
 
     same_products = Product.objects.all()[1:3]
