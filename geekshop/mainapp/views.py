d79439bc0533de7af1f062ba45cc7f8639bc71e8
GigGun <klyushinekb@gmail.com>
2019-05-28 20:06:10 +0500

    add hot product

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import get_object_or_404
 from .models import ProductCategory, Product
 from basketapp.models import BasketSlot
 
+import random as rnd
+
 context = {'title': 'Вагончик',
            'links_menu': {'main': 'Главная', 'products': 'Каталог', 'contact': 'Контакты'},
            'category': ProductCategory.objects.all(),
@@ -26,6 +28,9 @@ def products(request, pk=None):
 
     title = 'продукты'
     links_menu = ProductCategory.objects.all()
+    basket = get_basket(request.user)
+
+
 
     if pk is not None:
         if pk == 0:
@@ -42,15 +47,35 @@ def products(request, pk=None):
                    'products': products_obj, }
         return render(request, 'mainapp/product_list.html', context)
 
-    same_products = Product.objects.all()[1:3]
+    hot_product = get_hot_product()
+    same_products = get_same_prducts(hot_product)
 
     context = {'title': title,
                'links_menu': links_menu,
+               'hot_product': hot_product,
                'products': same_products,
-               'basket_quantity': total_quantity, }
+               'basket': basket, }
 
     return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
     return render(request, 'mainapp/contact.html', context)
+
+
+def get_basket(user):
+    if user.is_authenticated:
+        return BasketSlot.objects.filter(user=user)
+    else:
+        return []
+
+
+def get_hot_product():
+    products = Product.objects.all()
+    return rnd.sample(list(products), 1)[0]
+
+
+def get_same_prducts(hot_product):
+    same_products = Product.objects.filter(category=hot_product.category).\
+                        exclude(pk=hot_product.pk)[:3]
+    return same_products
