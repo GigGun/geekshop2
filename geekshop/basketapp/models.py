c768b72942be529b937c41cd51e59196f31c6756
GigGun <klyushinekb@gmail.com>
2019-05-28 19:47:43 +0500

    add some methods in basket model

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -16,3 +16,22 @@ class BasketSlot(models.Model):
 
     def __str__(self):
         return self.user.username + ' - ' + self.product.name
+
+    @property
+    def product_cost(self):
+        """return cost of all products"""
+        return self.product.price * self.quantity
+
+    @property
+    def total_quantity(self):
+        """return total quantity for user"""
+        _items = BasketSlot.objects.filter(user=self.user)
+        _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
+        return _totalquantity
+
+    @property
+    def total_cost(self):
+        """return total cost for user"""
+        _items = BasketSlot.objects.filter(user=self.user)
+        _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
+        return _totalcost
