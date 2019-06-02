09b6290a4a725ce6c01e9e71e95f9303ae2678ec
GigGun <klyushinekb@gmail.com>
2019-06-02 17:24:00 +0500

    correct remove product

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from .views import basket, add, delete
+from .views import basket, add, delete, basket_edit
 
 app_name = 'basketapp'
 
 urlpatterns = [
     path('', basket, name='view'),
     path('add/product/<int:pk>/', add, name='add'),
-    path('delete/product/<int:pk>/', delete, name='delete'),
-    path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit'),
+    path('remove/product/<int:pk>/', delete, name='delete'),
+    path('edit/<int:pk>/<int:quantity>/', basket_edit, name='edit'),
 ]
