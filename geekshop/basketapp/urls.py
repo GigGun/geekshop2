ec4ebd230acff8d4cb57032dcd66240506e8c7ee
GigGun <klyushinekb@gmail.com>
2019-06-02 16:19:49 +0500

    add edit product controller

--- geekshop/basketapp/urls.py
+++ geekshop/basketapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', basket, name='view'),
     path('add/product/<int:pk>/', add, name='add'),
     path('delete/product/<int:pk>/', delete, name='delete'),
+    path('edit/<int:pk>/<int:quantity>/', basketapp.basket_edit, name='edit'),
 ]
