30a581a4a700299b4ff59cb5c7f145e89268c026
GigGun <klyushinekb@gmail.com>
2019-06-08 11:51:46 +0500

    add new function

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,8 +1,16 @@
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
+
 from authapp.models import ShopUser
-from django.shortcuts import get_object_or_404, render
+from authapp.forms import ShopUserRegisterForm
+from authapp.forms import ShopUserAdminEditForm
+
 from mainapp.models import Product, ProductCategory
 
 
+
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'админка/пользователи'
     users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
@@ -16,15 +24,57 @@ def users(request):
 
 
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+    else:
+        user_form = ShopUserRegisterForm
+
+    context = {
+        'title': title,
+        'update_form': user_form, }
+    return render(request, 'adminapp/user_update.html', context)
 
 
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактироваие'
+
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.POST, request.FILES, instance=edit_user)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    context = {'title':title, 'update_form': edit_form}
+
+    return render(request, 'adminapp/user_update.html', context)
 
 
 def user_delete(request, pk):
-    pass
+    title = 'пользователи/удаление'
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        # user.delete()
+        # вместо удаления лучше сделаем неактивным
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    context = {
+        'title': title,
+        'user_to_delete': user, }
+
+    return render(request, 'adminapp/user_delete.html', context)
 
 
 def categories(request):
